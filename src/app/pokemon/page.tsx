c4da7ac64773ee07d38d456abcc2ae7aaafb7c09
MattFr <matthieu_franck@orange.fr>
2024-05-22 17:48:48 +0200

    test fixing suspense

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import Pokemon from '@/components/Pokemon'
-import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Pokemon from '@/components/Pokemon'
+
+function PokemonContent() {
+  const searchParams = useSearchParams()
+  const pokemonId = Number(searchParams.get('id'))
+  return <Pokemon pokemonId={pokemonId} />
+}
 
 export default function PokemonView() {
-  const searchParams = useSearchParams()
-
-  const pokemonId = Number(searchParams.get('id'))
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Pokemon pokemonId={pokemonId} />
+      <PokemonContent />
     </Suspense>
   )
 }
