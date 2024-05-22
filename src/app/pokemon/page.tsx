0fbb511dc80e53a83def2c22782d8bb5ca5f17dd
MattFr <matthieu_franck@orange.fr>
2024-05-22 17:43:52 +0200

    added suspense to pagetsx

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -2,10 +2,15 @@
 
 import Pokemon from '@/components/Pokemon'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function PokemonView() {
   const searchParams = useSearchParams()
 
   const pokemonId = Number(searchParams.get('id'))
-  return <Pokemon pokemonId={pokemonId} />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Pokemon pokemonId={pokemonId} />
+    </Suspense>
+  )
 }
